4685ae5f068284ea865d4a27ea89da6c4df40897
goldenxgpacking-rgb <goldenxgpacking@mail.com>
2026-05-24 22:07:56 +0630

    fix: unify JVM target to 17 for Java/Kotlin compatibility (Build #30)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     compileOptions {
         // Enable core library desugaring for flutter_local_notifications compatibility
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
